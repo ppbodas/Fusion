57e6d2e9a6b223af71166ebc632b81c595ea443f
Prathmesh <prathmesh@gmail.com>
2014-12-18 16:08:55 +0530

    Added admin role

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @postings = Posting.last(6).reverse
+    @postingsAll = Posting.where(:accepted=> "false").order("created_at desc")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141218081730) do
+ActiveRecord::Schema.define(:version => 20141218094358) do
 
   create_table "postings", :force => true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20141218081730) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
